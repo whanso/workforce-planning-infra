606fab0154c219645ea454ab35ad52df21e65d78
Will Hanson <w_hanson@outlook.com>
2024-01-26 15:19:57 -0500

    Add react front end.

--- workforce_planning_infra/workforce_planning_infra_stack.py
+++ workforce_planning_infra/workforce_planning_infra_stack.py
@@ -6,21 +6,55 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as lambda_,
+    aws_apigateway as apigateway,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deployment,
 )
 
 
 class WorkforcePlanningInfraStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "WorkforcePlanningInfraQueue",
-            visibility_timeout=Duration.seconds(300),
+        handler = lambda_.Function(
+            self,
+            "WorkforcePlanningInfraFunction",
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            code=lambda_.Code.from_asset("lambda"),
+            handler="lambda_handler.handler",
         )
 
-        topic = sns.Topic(
-            self, "WorkforcePlanningInfraTopic"
+        apigateway.LambdaRestApi(
+            self,
+            "WorkforcePlanningInfraApi",
+            handler=handler,
+            deploy=True,
+            default_cors_preflight_options=apigateway.CorsOptions(
+                allow_origins=apigateway.Cors.ALL_ORIGINS,
+                allow_methods=apigateway.Cors.ALL_METHODS,
+                allow_headers=["*"],
+            ),
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        bucket = s3.Bucket(
+            self,
+            "WorkforcePlanningInfraBucket",
+            bucket_name="WorkforcePlanningInfraBucket",
+            public_read_access=True,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
+            access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+            website_index_document="index.html",
+        )
+
+        s3_deployment.BucketDeployment(
+            self,
+            "StaticWebsite",
+            sources=[s3_deployment.Source.asset("./react-ts/build")],
+            destination_bucket=bucket,
+        )
